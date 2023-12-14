0d51a29f7e86e64a15e5283fb7e99410f84680ca
kaare <109585723+kaareskokebok@users.noreply.github.com>
2023-12-14 13:25:19 +0100

    add template index

--- JavaScript/10 - Hotellbooking/script.js
+++ JavaScript/10 - Hotellbooking/script.js
@@ -3,17 +3,6 @@
 // arne bor på rom 4 (index 3)
 // De andre rommene er ledige
 let rommene = ["jens","","","arne",""];
-/*
-Steg 1: Lag en oversiktsfunksjon med en knapp i HTML.
-Utseendet på utskriften
-Rom 1: jens
-Rom 2: ledig
-Rom 3: ledig
-osv...
 
-Steg 2: Legg til mulighet for å booke rom med navn. Gjesten skal plasseres
-på første ledige rom. Hvis ingen rom er ledige, gis feilmelding.
 
-Steg 3: Legg til sjekk ut. Navn og romnr må oppgis, og stemme overens 
-med informasjonen i rommene-arrayet. 
-*/
+
