42c5c8dce2303448cf3b307e7b98dcc2baffc054
kaare <109585723+kaareskokebok@users.noreply.github.com>
2023-12-18 14:49:49 +0100

    rom og gjestedatabase ok. neste: vis oversikt

---
+++ JavaScript/10 - Hotellbooking/Med objektarray/hotelldatabase.js
@@ -0,0 +1,27 @@
+let rommene = [
+    { romNr: 1, status: "opptatt", type: "enkeltrom", pris: 2270 },
+    { romNr: 2, status: "ledig", type: "junior suite", pris: 5460 },
+    { romNr: 3, status: "rengjøring", type: "dobbeltrom", pris: 2870 },
+    { romNr: 4, status: "opptatt", type: "enkeltrom", pris: 2300 },
+    { romNr: 5, status: "opptatt", type: "suite", pris: 5600 },
+    { romNr: 6, status: "opptatt", type: "dobbeltrom", pris: 2900 },
+    { romNr: 7, status: "rengjøring", type: "enkeltrom", pris: 2200 },
+    { romNr: 8, status: "opptatt", type: "dobbeltrom", pris: 3000 },
+    { romNr: 9, status: "ledig", type: "junior suite", pris: 5500 },
+    { romNr: 10, status: "rengjøring", type: "suite", pris: 6000 },
+    { romNr: 11, status: "opptatt", type: "enkeltrom", pris: 2400 },
+    { romNr: 12, status: "ledig", type: "dobbeltrom", pris: 3100 },
+    { romNr: 13, status: "ledig", type: "junior suite", pris: 5300 },
+    { romNr: 14, status: "rengjøring", type: "suite", pris: 6100 },
+    { romNr: 15, status: "ledig", type: "dobbeltrom", pris: 3200 }
+];
+
+
+let gjestene = [
+    { fornavn: "Jens", etternavn: "Jensen", alder: 48, epost: "jens@mail.no", romNr: 1 },
+    { fornavn: "Arne", etternavn: "Persen", alder: 35, epost: "arne@mail.no", romNr: 4 },
+    { fornavn: "Lise", etternavn: "Larsen", alder: 28, epost: "lise@mail.no", romNr: 5 },
+    { fornavn: "Kari", etternavn: "Karlsen", alder: 42, epost: "kari@mail.no", romNr: 6 },
+    { fornavn: "Ola", etternavn: "Olsen", alder: 55, epost: "ola@mail.no", romNr: 8 },
+    { fornavn: "Eva", etternavn: "Eriksen", alder: 33, epost: "eva@mail.no", romNr: 11 }
+];
--- JavaScript/10 - Hotellbooking/Med objektarray/script.js
+++ JavaScript/10 - Hotellbooking/Med objektarray/script.js
@@ -52,7 +52,6 @@ function forsteLedigeIndex(){
 // I begynnelsen bor jens på rom 1 (index 0)
 // arne bor på rom 4 (index 3)
 // De andre rommene er ledige
-let rommene = ["jens", "", "", "arne", ""];
 // rommene og gjestene er objektarray fra hotelldatabase.js
 
 
