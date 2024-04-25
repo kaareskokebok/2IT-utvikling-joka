0871163f56df3406e0a77611c3153023f2079823
kaare <109585723+kaareskokebok@users.noreply.github.com>
2024-04-25 10:42:04 +0200

    halvferdig bursdag

--- EJS_og_node/EJS-bursdag-inv/index.js
+++ EJS_og_node/EJS-bursdag-inv/index.js
@@ -4,10 +4,14 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+    // navn, navnGjest, alder, tid, sted
     res.render("index.ejs", 
         {
             navn: "Sara",
-            // Osv.
+            navnGjest: "Per",
+            alder: 25,
+            tid: "kl. 1300 på lørdag",
+            sted: "Jessheimbadet"
         }
     );
 
