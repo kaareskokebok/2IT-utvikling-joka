3fbdd85c244b3051807e96cecc0f8869e8c93270
kaare <109585723+kaareskokebok@users.noreply.github.com>
2024-05-30 11:00:42 +0200

    navnene

--- finalen/chat-app/app.js
+++ finalen/chat-app/app.js
@@ -12,19 +12,22 @@ app.use(express.urlencoded({ extended: true }));
 
 // Meldinger lagres i et array
 let meldinger = ["Morn dere", "Har dere gjort leksene", "Nei, det var kjedelig."];
+let navnene = ["Per", "Jens", "Sara"];
 
 app.get("/", (req, res) => {
     // Vise fram index.ejs ved localhost:3000/
-    res.render("index.ejs", {meldinger}); //{meldinger: meldinger}
+    res.render("index.ejs", {meldinger, navnene}); //{meldinger: meldinger}
 });
 // Skjema
 app.post("/nymelding", (req, res) => {
     console.log(req.body);
     // 1. Legg til nyeste melding i slutten av arrayet meldinger
     let nyMelding = req.body.melding;
+    let navn = req.body.navn;
     meldinger.push(nyMelding);
+    navnene.push(navn);
     // 2. Kjør res.render med meldinger som data, og index.ejs som side.
-    res.render("index.ejs", {meldinger});
+    res.render("index.ejs", {meldinger, navnene});
 });
 
 app.listen(port, () => {
