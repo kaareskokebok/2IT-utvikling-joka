01d46a8bab55a4ba0b3ed9bdaf7de0eea2403773
kaare <109585723+kaareskokebok@users.noreply.github.com>
2024-05-30 09:16:40 +0200

    app.js basic ok

---
+++ finalen/chat-app/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// Henter stilfiler, js-filer, bilder osv. fra public
+app.use(express.static("public"));
+// Lar oss bruke req.body for å hente skjemadata
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    // Vise fram index.ejs ved localhost:3000/
+    res.render("index.ejs");
+});
+// Skjema
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+    let data = {
+        melding: req.body.melding
+    };
+    res.render("formSubmit.ejs", data);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
